0b3abaa6fd67d04968fb6f3af9af44cc7f83d584
L R <ljnetwerk@posteo.nl>
2022-09-05 18:07:26 +0200

    split funcs

--- go/main.go
+++ go/main.go
@@ -2,139 +2,150 @@ package main
 
 import (
 	"context"
-  "encoding/json"
-  "io/ioutil"
-  "os"
-	"fmt"
-  "github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-	"time"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 type App struct {
-  Name  string `json:"name"`
-  Url   string `json:"url"`
-  Icon  string `json:"icon"`
+	name string `json:"name"`
+	url  string `json:"url"`
+	icon string `json:"icon"`
 }
 
-type Apps struct {
-  Apps []App `json:"apps"`
-}
+func main() {
 
-func main(){
-	filename := flag.String("apps-config", "/config/apps.json", "Location of apps.json file")
-	mode := flag.String("run-mode", "interval", "Run mode (interval vs. once)")
-	interval := flag.Int("check-interval", 30, "Interval in seconds for checking container labels")
+	outputFile := flag.String("apps-config", "/config/apps.json", "Location of apps.json file")
 	flag.Parse()
 
-  checkFileExists(*filename)
+	log.Println("Starting run")
 
-	if *mode == "once" {
-		fmt.Println()
-		fmt.Println("Starting run")
-		fmt.Println("---------------------")
-		fmt.Println()
-		updateJson(*filename)
-		fmt.Println()
-		fmt.Println("---------------------")
-		fmt.Println("Stopping run")
-		fmt.Println()
-	} else if *mode == "interval" {
-		for {
-			fmt.Println()
-			fmt.Println("Starting run")
-			fmt.Println("---------------------")
-			fmt.Println()
-	    updateJson(*filename)
-			fmt.Println()
-			fmt.Println("---------------------")
-			fmt.Println("Stopping run")
-			fmt.Println()
-			time.Sleep(time.Duration(*interval) * time.Second)
-	  }
-	} else {
-		fmt.Println("Unknown run mode")
+	err := writeAppsFile(*outputFile)
+
+	if err != nil{
+		log.Fatal(err.Error())
 	}
+
+	log.Println("Stopping run")
 }
 
-func checkFileExists(filename string) error {
-    _, err := os.Stat(filename)
-        if os.IsNotExist(err) {
-            _, err := os.Create(filename)
-                if err != nil {
-                    return err
-                }
-        }
-        return nil
+func writeAppsFile(outputFile string) error {
+
+	err := createFileIfNotExists(outputFile)
+	if err != nil {
+		return err
+	}
+
+	containers, err := getContainers()
+	if err != nil{
+		return err
+	}
+	
+	apps := []App{}
+	for _, container := range containers {
+		app := newApp{container}
+
+		// If app is empty, we do not append
+		if app == App {
+			log.Println("Container has no sui labels")
+			continue
+		}
+		append(apps, app)
+	}
+
+	err := apps.toJson(outputFile)
+
+	if err != nil{
+		return err
+	}
+
+	return nil
 }
 
+func newApp(c container) app {
+	log.Println("Parsing labels from container:", strings.Trim(fmt.Sprint(c.Names), "/[]"))
+	app := App{}
 
-func updateJson(filename string){
-	  containers := getContainers()
+	app.name = parseName(container.Labels)
+	app.url = parseUrl(container.Labels)
+	app.icon = parseIcon(container.Labels)
 
-	  apps_empty := []App{}
-		apps := Apps{apps_empty}
-
-	  for _, container := range containers {
-			fmt.Println("---")
-			fmt.Println("Found container with name:", strings.Trim(fmt.Sprint(container.Names), "/[]"))
-			app := App{}
-
-	    for key, value := range container.Labels{
-	      if key == "sui.app.name" {
-					fmt.Printf("Container label sui.app.name: %s\n", value)
-	        app.Name = value
-	      }
-	      if key == "sui.app.url" {
-	        app.Url = value
-					fmt.Printf("Container label sui.app.url: %s\n", value)
-	      }
-	      if key == "sui.app.icon" {
-	        app.Icon = value
-					fmt.Printf("Container label sui.app.icon: %s\n", value)
-	      }
-	    }
-			if (App{}) != app  {
-	    	  apps.AddItem(app)
-			} else {
-				fmt.Println("Container has no sui labels")
-			}
-			fmt.Println("---")
-	  }
-
-	  writeJson(filename, apps)
+	return app
 
 }
 
-func getContainers() []types.Container {
-  ctx := context.Background()
+func parseName(labels container.Labels) string {
+	for key, value := range labels {
+		if key == "sui.app.name" {
+			log.Printf("Container label sui.app.name: %s\n", value)
+			return value
+		}
+	}
+	return ""
+}
+
+func parseUrl(labels container.Labels) string {
+	for key, value := range labels {
+		if key == "sui.app.url" {
+			log.Printf("Container label sui.app.url: %s\n", value)
+			return value
+		}
+	}
+	return ""
+}
+
+func parseIcon(labels container.Labels) string {
+	for key, value := range labels {
+		if key == "sui.app.icon" {
+			log.Printf("Container label sui.app.icon: %s\n", value)
+			return value
+		}
+	}
+	return ""
+}
+
+func getContainers() ([]types.Container, error) {
+	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		panic(err)
+		return []types.Container, err
 	}
 
-  containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-  if err != nil {
-    panic(err)
-  }
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return []types.Container, err
+	}
 
-  return containers
+	return containers, nil
 
 }
 
-func (apps *Apps) AddItem(app App) []App {
-	apps.Apps = append(apps.Apps, app)
-	return apps.Apps
+func createFileIfNotExists(file string) error {
+	_, err := os.Stat(file)
+	if os.IsNotExist(err) {
+		_, err := os.Create(file)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func writeJson(filename string, apps Apps){
-  dat, err := json.MarshalIndent(apps, "", "    ")
-  if err != nil {
-    panic(err)
-  }
+func (apps Apps) toJson(file string) error {
+	dat, err := json.MarshalIndent(apps, "", "    ")
+	if err != nil {
+		retur err
+	}
 
-  err = ioutil.WriteFile(filename, dat, 0644)
-
+	err = ioutil.WriteFile(file, dat, 0644)
+	if err != nil{
+		return err
+	}
 }
